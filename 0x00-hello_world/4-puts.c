1a2c044c48d606237b9a960364e5690b4bbd4de0
Muktar <mukhtarie3@gmail.com>
2023-06-15 10:45:10 -0700

    C programming is like a multilingual language, the very first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,22 @@
+/**
+ * File: 4-puts.c
+ * Author: Muktar
+ * Date:   15th June 2023
+ *
+ * Description: A program that prints a sentence using the puts function.
+ */
+
 #include <stdio.h>
+
+	/**
+	 * main - Entry point of the program
+	 *
+	 * Description: Prints "Programming is like building a multilingual puzzle."
+	 *
+	 * Return: Always 0
+	 */
 int main(void)
-	{
-	puts("\"Programming is like building a multilingual puzzle");
+{
+	("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
