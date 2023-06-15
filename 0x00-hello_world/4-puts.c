dcc6bea5f2b627f87b72a8657c7e442ec13e7896
Muktar <mukhtarie3@gmail.com>
2023-06-15 11:59:19 -0700

    using the puts function, C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function serves as the entry point of the program.
+ * It prints the sentence
+ * "Programming is like building a multilingual puzzle."
+ * using the `puts` function.
+ *
+ * Return:
+ *   The function returns an exit status of 0 to indicate successful execution.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
