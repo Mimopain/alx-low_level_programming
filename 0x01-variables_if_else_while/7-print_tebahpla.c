3635f6f7b7863b226f10952d19743e9bbfe14480
Muktar <mukhtarie3@gmail.com>
2023-06-16 12:32:28 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the tebahpla in lowercase using
+ * the putchar function.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter++)
+
+	{
+		putchar(letter);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
