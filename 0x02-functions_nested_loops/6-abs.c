0d8e0083821877846af1ff7b4e90c610a083a599
Muktar <mukhtarie3@gmail.com>
2023-06-20 09:10:45 -0700

    absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: a program that calculates the absolute value of an integer
+ *@int: argument
+ * Return: return 0 on success
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = -n;
+			return (n);
+}
