a3bd0d3429e1ab72efb9b9c650e19e70357b6c16
Muktar <mukhtarie3@gmail.com>
2023-06-20 13:45:45 -0700

    add

---
+++ 10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * add - function
+ *
+ * Description: add two integers
+ *
+ * @a: character declaration
+ * @b: character declaration
+ * Return: return zero
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
