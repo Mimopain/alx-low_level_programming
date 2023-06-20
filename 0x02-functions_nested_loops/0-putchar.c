9ed5ea0b031b6e5a76d7fc49d7b225d11e63b854
Muktar <mukhtarie3@gmail.com>
2023-06-20 04:16:34 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: - A program to print all the characters of _putchar
+ * followed by a new line
+ *
+ * Return: - return 0 when succesful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
