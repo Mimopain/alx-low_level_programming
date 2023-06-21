d1feb4188936cbda8f0f82197054c9e4ad38b9c3
Muktar <mukhtarie3@gmail.com>
2023-06-20 17:17:32 -0700

    fibonacci 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,18 +14,18 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 1, fib2 = 2, next;
-	
+
 	printf("%lu, %lu", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 		next = fib1 + fib2;
-        printf(", %lu", next);
+
+		printf(", %lu", next);
 
 	fib1 = fib2;
 	fib2 = next;
-    }
+	}
 	printf("\n");
-
-    return (0);
+	return (0);
 }
