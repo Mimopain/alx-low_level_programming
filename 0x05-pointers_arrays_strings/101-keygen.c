0a20231f2967a3cc6ab5a8c70eee90b74ed10a7d
Muktar <mukhtarie3@gmail.com>
2023-06-26 13:03:27 -0700

    random passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+/**
+ * generate_password - Generates a random password.
+ *
+ * Return: The generated password as a string.
+ */
+
+char *generate_password(void)
+{
+	static char password[PASSWORD_LENGTH + 1];
+	const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int i;
+
+	srand(time(0));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = characters[rand() % (sizeof(characters) - 1)];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+
+	return (password);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", generate_password());
+	return (0);
+}
