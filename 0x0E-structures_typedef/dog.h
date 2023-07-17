0e3a6b8f9ece07510ccdff4e6ee269f05d84fd9c
Muktar <mukhtarie3@gmail.com>
2023-07-17 08:39:04 -0700

    initialize struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Represents a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner of the dog
+ *
+ * Description: This struct represents a dog with its name, age, and owner.
+ */
 struct dog
 {
 	char *name;
@@ -8,6 +16,16 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function initializes the members of the struct dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif /* DOG_H */
-
