fd980ef4b85580cd27d5f2c20f60bfde6c698589
Muktar <mukhtarie3@gmail.com>
2023-07-27 10:40:22 -0700

    program in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,10 @@
-section .data
-    hello_holberton db 'Hello, Holberton', 0  ; Null-terminated string
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    ; Set up the stack frame (for compatibility with C functions)
-    push rbp
-    mov rbp, rsp
-
-    ; Call printf function to print the string
-    mov rdi, hello_holberton  ; Pass the address of the string as the first argument (format string)
-    xor rax, rax              ; Clear rax (to indicate no vector registers used)
-    call printf               ; Call the printf function
-
-    ; Clean up the stack frame and return
-    leave
-    ret
-
-section .bss
-    ; Empty BSS section to satisfy the linker
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
