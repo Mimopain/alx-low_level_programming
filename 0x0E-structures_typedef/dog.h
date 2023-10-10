8572a1832118f67e33e00cb3edf3ff69da4f8a3a
Muktar <mukhtarie3@gmail.com>
2023-10-10 20:27:50 +0100

    Preprocessor and Structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner of the dog
+ *
+ * Description: This struct represents a dog with its name, age, and owner.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t;
+
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function initializes the members of the struct dog.
+ */
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(const dog_t *d);dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif /* DOG_H */
