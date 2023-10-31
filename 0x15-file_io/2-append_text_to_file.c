f986401d13f1c10ffacb918ec5e3842d20ebdb5a
Muktar <mukhtarie3@gmail.com>
2023-10-30 21:59:20 -0700

    betty fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,33 +9,32 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, w_status;
-    ssize_t len = 0, w_bytes;
+	int fd, w_status;
+	ssize_t len = 0, w_bytes;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content == NULL)
-        return (1);
+	if (text_content == NULL)
+		return (1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    while (text_content[len] != '\0')
-        len++;
+	if (fd == -1)
+		return (-1);
 
-    w_bytes = write(fd, text_content, len);
-    if (w_bytes == -1 || w_bytes != len)
-    {
-        close(fd);
-        return (-1);
-    }
+	while (text_content[len] != '\0')
+		len++;
 
-    w_status = close(fd);
-    if (w_status == -1)
-        return (-1);
+	w_bytes = write(fd, text_content, len);
+	if (w_bytes == -1 || w_bytes != len)
+	{
+		close(fd);
+		return (-1);
+	}
+	w_status = close(fd);
+	if (w_status == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
-
