d073999b37ffa85bf1c5a17c4b0a0170aa4834df
Muktar <mukhtarie3@gmail.com>
2023-12-12 04:28:02 +0100

    adding files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	return (l);
+}
